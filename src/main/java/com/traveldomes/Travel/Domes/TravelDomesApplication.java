a5341183435429bae074edbf8e73c89c025e3783
ferdiany19 <ferdianyuliansyah19@gmail.com>
2023-10-26 17:09:33 +0700

    initial commit

---
+++ src/main/java/com/traveldomes/Travel/Domes/TravelDomesApplication.java
@@ -0,0 +1,13 @@
+package com.traveldomes.Travel.Domes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traveldomes/Travel/Domes/TravelDomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveldomes.Travel.Domes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
